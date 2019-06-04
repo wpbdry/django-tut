bf6269775a9ac6a1898c4855c45631ca3216e2e3
William Dry <wpbdry@gmail.com>
2019-06-04 15:15:47 +0200

    Setup views

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -5,9 +5,18 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
+    def __str__(self):
+        return '{0}, Date: {1}'.format(self.question_text, str(self.pub_date))
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
-    
+
+    def __str__(self):
+        return '\nQuestion: {0},\nChoice: {1}, Votes: {2}\n'.format(
+            self.question,
+            self.choice_text,
+            self.votes
+        )
